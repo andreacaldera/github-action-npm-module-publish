2de43983c68e6938d44727990d939427412fa112
Andrea Caldera <andrea.caldera@gmail.com>
2021-09-21 13:32:45 +0100

    tag version

--- index.js
+++ index.js
@@ -31,30 +31,11 @@ Toolkit.run(async (tools) => {
   ]);
 
   const pkg = tools.getPackageJSON();
+  const { version } = pkg;
+  const tag = `v${version}`;
 
-  console.log(`Current version is ${pkg.version}`);
+  console.log(`Current version is ${version} and tag ${tag}`);
 
-  const pkgMain = JSON.parse(await execSync(`git show origin/main:package.json`));
-  const mainVersion = pkgMain.version;
-  console.log(`Main version ${mainVersion}`);
-
-  const bumpVersion = async (releaseType) => {
-    console.log(`${releaseType} release`);
-    await runCommand(`yarn version --${releaseType} --no-git-tag-version`);
-    await runCommand(`git add package.json`);
-    const newVersion = JSON.parse(fs.readFileSync('package.json')).version;
-    await runCommand(`git commit -m 'ci: v${newVersion}'`);
-    await runCommand(`git push origin ${currentBranch}`);
-  };
-
-  fs.writeFileSync('package.json', JSON.stringify({ ...pkg, version: mainVersion }, null, 2));
-  const currentBranch = /refs\/[a-zA-Z]+\/(.*)/.exec(process.env.GITHUB_REF)[1];
-  const commits = await runCommand(`git log origin/main...${currentBranch} --oneline`);
-  if (commits.includes('major')) {
-    await bumpVersion('major');
-  } else if (commits.includes('minor')) {
-    await bumpVersion('minor');
-  } else {
-    await bumpVersion('patch');
-  }
+  await runCommand(`git tag -a ${tag} -m "Version ${ve}"`);
+  await runCommand(`git push origin ${tag}`);
 });
